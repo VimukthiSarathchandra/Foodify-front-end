4d4afd9c61fd3938544066b737e85440a8fc6d22
Vimukthi Sarathchandra <sarathchandravimukthi@gmail.com>
2025-09-14 03:38:46 +0530

    create sign up page

--- src/main.tsx
+++ src/main.tsx
@@ -2,10 +2,17 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import SignInPage from './pages/sign-in.page'
+import { BrowserRouter, Route, Routes } from "react-router";
+import SignUpPage from './pages/sign-up.page';
 
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <SignInPage />
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<SignInPage />} />
+        <Route path="/sign-up" element={<SignUpPage />} />
+      </Routes>
+    </BrowserRouter>
   </StrictMode>,
 )
