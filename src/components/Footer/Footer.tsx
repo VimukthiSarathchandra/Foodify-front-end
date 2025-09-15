4f4d6e1123424948ab6480c020a146a178c05c68
Vimukthi Sarathchandra <sarathchandravimukthi@gmail.com>
2025-09-15 17:07:39 +0530

    footer complite

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -1,9 +1,69 @@
+import { Link } from "react-router";
+import { Facebook, Instagram, Twitter, Send } from "lucide-react";
+
 function Footer() {
   return (
-    <div>
-      <h1>Footer</h1>
-    </div>
-  )
+    <footer className="bg-white py-8 px-4 md:px-10 lg:px-30">
+      <div className="flex flex-col md:flex-row justify-between items-start md:items-center max-w-6xl mx-auto">
+        <div className="flex flex-col items-start space-y-4 mb-6 md:mb-0">
+          <div className="flex space-x-4">
+            <Twitter className="w-6 h-6 text-[#59423C] hover:text-[#FE724C] cursor-pointer" />
+            <Facebook className="w-6 h-6 text-[#59423C] hover:text-[#FE724C] cursor-pointer" />
+            <Send className="w-6 h-6 text-[#59423C] hover:text-[#FE724C] cursor-pointer" />
+            <Instagram className="w-6 h-6 text-[#59423C] hover:text-[#FE724C] cursor-pointer" />
+          </div>
+
+          <p className="text-sm mt-5 md:mt-10 text-[#59423C] ">
+            © 2020 Foodify. All rights reserved
+          </p>
+        </div>
+
+        <div className="flex space-x-20  md:flex-row space-y-4 md:space-y-0 md:space-x-12">
+          <div className="flex flex-col space-y-2">
+            <h3 className=" font-bold text-lg text-[#59423C]">Foods</h3>
+            <div className="flex flex-col space-y-1">
+              <Link
+                to="/pricing"
+                className="text-[#59423C] hover:text-[#FE724C] text-sm"
+              >
+                Pricing
+              </Link>
+              <Link
+                to="/menus"
+                className="text-[#59423C] hover:text-[#FE724C] text-sm"
+              >
+                Menus
+              </Link>
+            </div>
+            <Link
+              to="/privacy"
+              className="text-[#59423C] hover:text-[#FE724C] text-sm mt-4"
+            >
+              Privacy Policy
+            </Link>
+          </div>
+
+          <div className="flex flex-col space-y-2">
+            <h3 className=" font-bold text-lg text-[#59423C]">Help</h3>
+            <div className="flex flex-col space-y-1">
+              <Link
+                to="/about"
+                className="text-[#59423C] hover:text-[#FE724C] text-sm"
+              >
+                About
+              </Link>
+            </div>
+            <Link
+              to="/terms"
+              className="text-[#59423C] hover:text-[#FE724C]  text-sm mt-10 "
+            >
+              Terms & Conditions
+            </Link>
+          </div>
+        </div>
+      </div>
+    </footer>
+  );
 }
 
 export default Footer;
--- src/components/Subscribe.tsx
+++ src/components/Subscribe.tsx
@@ -1,20 +1,29 @@
 import { Button } from "./ui/button";
 
-
 function Subscribe() {
   return (
-  <>
-  <div className="flex flex-col space-y-5 mt-10 mb-15 px-4 md:px-10 lg:px-30 relative z-10">
+    <>
+      <div className="flex flex-col space-y-5 mt-10 mb-15 px-4 md:px-10 lg:px-30 relative z-10">
+        <img
+          src="/assets/images/rectangle.png"
+          alt="Subscribe"
+          className="md:w-full md:h-auto object-cover h-75 rounded-4xl"
+        />
+        <div className="absolute flex flex-col space-y-2 md:space-y-5 md:mt-10 mt-10  lg:mt-25 mb-15 px-4 md:px-10 lg:px-15">
+          <h2 className=" text-[#FE724C] ">SUBSCRIBE</h2>
+          <p className=" md:text-4xl text-2xl font-medium">
+            Perfect Solutions <br /> For Your Foods
+          </p>
+          <p className="text-gray-500">
+            Our mission is to provide healthy,
+            <span className="block md:inline"> flavorful, and convenient food</span>
+            <span className="block"> options for your busy lifestyle.</span>
+          </p>
 
-    <img src="/assets/images/rectangle.png" alt="Subscribe" className="md:w-full md:h-auto object-cover h-75 rounded-4xl"/>
-    <div className="absolute flex flex-col space-y-2 md:space-y-5 md:mt-10 mt-10  lg:mt-25 mb-15 px-4 md:px-10 lg:px-15">
-      <h2 className=" text-[#FE724C] ">SUBSCRIBE</h2>
-      <p className=" md:text-4xl text-2xl font-medium">Perfect Solutions <br /> For Your Foods</p>
-      <p className="text-gray-500">Our mission is to provide healthy,  flavorful, and convenient <br />food options for your busy lifestyle.</p>
-      <Button className="bg-[#FE724C] text-white w-1/4">Subscribe</Button>
-    </div>
-  </div>
-  </>
+          <Button className="bg-[#FE724C] text-white w-1/3 md:w-1/4">Subscribe</Button>
+        </div>
+      </div>
+    </>
   );
 }
 
